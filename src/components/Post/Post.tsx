567f043588617f566c04a81fdddc12f07176811f
taehyeungkim98 <kth1837@snu.ac.kr>
2023-02-12 08:18:50 +0000

    :sparkles: add comment bar component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,6 +2,7 @@
 import styles from './Post.module.scss'
 import PostIntro from './PostRelated/PostIntro/PostIntro';
 import PostContent from './PostRelated/PostContent/PostContent';
+import PostCommentBar from './PostRelated/PostCommentBar/PostCommentBar'
 
 interface PostProps {
     data: any;
@@ -12,6 +13,7 @@ function Post({data}:PostProps) {
         <div className = {styles.container}>
             <PostIntro photoURL={data.photo.url} title={data.content.title}/>
             <PostContent/>
+            <PostCommentBar/>
         </div>
     )
 }
---
+++ src/components/Post/PostRelated/PostCommentBar/PostCommentBar.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styles from './PostCommentBar.module.scss'
+import styles_upper from './PostCommentBarUpper.module.scss';
+import styles_below from './PostCommentBarBelow.module.scss';
+import {likeplus, share, exclamation, toggledown, toggleup, write} from '../../../../icons/icons'
+
+interface CommentBarUpperProps {
+    toggleBar: (arg:boolean)=>void,
+    visible: boolean;
+}
+
+function CommentBarUpper({toggleBar, visible}:CommentBarUpperProps) {
+    return(
+        <div className={styles_upper['bar_upper']}>
+            <button className={styles_upper.like}>{likeplus()}</button>
+            <button className={styles_upper.share}>{share()}</button>
+            <button className={styles_upper.exclamation}>{exclamation()}</button>
+            <button className={styles_upper.hideshowtoggle} onClick={()=>{
+                toggleBar(visible)
+            }}>{visible ? toggledown() : toggleup()}</button>            
+        </div>
+    )
+}
+
+interface CommentBarBelowProps {
+    visible:boolean;
+}
+
+function CommentBarBelow({visible}:CommentBarBelowProps) {
+    return(
+        <div className={styles_below['bar_down']}>
+            <div className={styles_below['bar_down--profile']}>
+                <figure className={styles_below['profile']}></figure>
+            </div>
+            <div className={styles_below['bar_down--comment']} style={visible ? undefined : {height: 0}}>
+                <textarea placeholder={"댓글을 입력해주세요"}></textarea>
+            </div>
+            <button className={styles_below['bar_down--submit']}>
+                {write()}
+            </button>
+        </div>
+    )
+}
+
+function PostCommentBar() {
+
+    const [barVisible, setBarVisible] = useState<boolean>(true);
+
+    const toggleBar = (visible:boolean) => {
+        setBarVisible(visible)
+    }
+    
+    return (
+    <div className = {styles.bar}>
+        <CommentBarUpper toggleBar={toggleBar} visible={barVisible}/>
+        <CommentBarBelow visible={barVisible}/>
+    </div>
+    )
+}
+export default PostCommentBar
