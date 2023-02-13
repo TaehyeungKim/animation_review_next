36b2f096dcfca048abc5038787b90549635a0d9b
taehyeungkim98 <kth1837@snu.ac.kr>
2023-02-13 11:57:43 +0000

    :art: add titles to thumbnail

--- src/pages/CreatePage/sub/ThumbnailSet.tsx
+++ src/pages/CreatePage/sub/ThumbnailSet.tsx
@@ -1,6 +1,30 @@
-import {useState, useRef, useEffect} from 'react';
+import {useState, useRef} from 'react';
 import styles from './ThumbnailSet.module.scss';
+import styles_title from './ThumbnailTitle.module.scss';
 import {imageAdd, trash} from '../../../icons/icons';
+
+
+
+function ThumbnailTitle() {
+	
+	const restorePlaceholder = (e:any) => {
+		if(e.target?.innerHTML === '') {
+			e.target?.classList.remove(`${styles_title['title--focused']}`)
+		} else {
+			
+		}
+	}
+	return(
+		<div className = {styles_title.title}>
+			<h1 className={styles_title['title--main']} contentEditable = 'true' onFocus={(e:any)=>{
+					e.target?.classList.add(`${styles_title['title--focused']}`);
+				}} onBlur = {(e:any)=>{
+					restorePlaceholder(e);
+				}}></h1>
+			<h3 className={styles_title['title--sub']} contentEditable = 'true'></h3>
+		</div>
+	)
+}
 
 function ThumbnailSet() {
 	const [thumbnail, setThumbnail] = useState<string|null>(null);
@@ -20,11 +44,9 @@ function ThumbnailSet() {
 		} else {console.log('no image')}
 	}
 	
-	// useEffect(()=>{
-		
-	// },[thumbnail])
 	return(
 		<section className={styles.thumbnail} style={thumbnail ? {backgroundImage: `url(${thumbnail})`} : undefined}>
+			<ThumbnailTitle/>
 			<aside className = {styles.thumbnail_optionbar}>
 				<button className={styles['thumbnail_optionbar--imageAdd']} onClick={()=>{
 						imageInp.current?.click();
