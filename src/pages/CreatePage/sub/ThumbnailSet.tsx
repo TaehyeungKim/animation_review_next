2047b2e00da96f17222c6afa74ba82d8c43e7195
taehyeungkim98 <kth1837@snu.ac.kr>
2023-02-13 10:51:28 +0000

    :sparkles: add create thumbnail

--- src/pages/CreatePage/sub/ThumbnailSet.tsx
+++ src/pages/CreatePage/sub/ThumbnailSet.tsx
@@ -1,17 +1,40 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import styles from './ThumbnailSet.module.scss';
-import {imageAdd} from '../../../icons/icons';
+import {imageAdd, trash} from '../../../icons/icons';
 
 function ThumbnailSet() {
-	
+	const [thumbnail, setThumbnail] = useState<string|null>(null);
 	const imageInp = useRef<HTMLInputElement>(null);
+	
+	const updateThumbnail = (e:any) => {
+		const targetFile = e.target?.files[0];
+		const tempURL = URL.createObjectURL(targetFile);
+		setThumbnail(tempURL);
+		 
+	}
+	const deleteThumbnail = () => {
+		if(imageInp.current) {
+			imageInp.current.value = '';
+			setThumbnail(null);
+			console.log(imageInp.current?.files)
+		} else {console.log('no image')}
+	}
+	
+	// useEffect(()=>{
+		
+	// },[thumbnail])
 	return(
-		<section className={styles.thumbnail}>
+		<section className={styles.thumbnail} style={thumbnail ? {backgroundImage: `url(${thumbnail})`} : undefined}>
 			<aside className = {styles.thumbnail_optionbar}>
 				<button className={styles['thumbnail_optionbar--imageAdd']} onClick={()=>{
 						imageInp.current?.click();
 					}}>{imageAdd()}</button>
-				<input type="file" accept="image/*" hidden ref={imageInp}/>
+				<input type="file" accept="image/*" hidden ref={imageInp} onChange = {(e:any)=>{
+						updateThumbnail(e);
+					}}/>
+				<button className = {styles['thumbnail_optionbar--imageDelete']} onClick = {()=>{
+						deleteThumbnail();
+					}}>{trash()}</button>
 			</aside>
 		</section>
 	)
