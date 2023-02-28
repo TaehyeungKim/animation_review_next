4e02b0d517748a50e8b4721a4236de757e81e7af
taehyeungkim98 <kth1837@snu.ac.kr>
2023-02-28 09:07:32 +0000

    :art: right side of the container in main page hanging on the right window side

--- src/components/Partition/Partition.tsx
+++ src/components/Partition/Partition.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import styles from './Partition.module.scss'
 import ArticleFeed from '../ArticleFeed/ArticleFeed'
 import {leftArrow, rightArrow} from '../../icons/icons'
@@ -22,54 +22,86 @@ const loadArticles = async(title: string, start: number, limit: number, settingR
 }
 function Partition({title}:PartitionProps) {
     const [resources, setResources] = useState<Array<any>>([]);
+    const [scrollIdx, setScrollIdx] = useState<number>(0);
+    const containerTranslated = useRef<number>(0);
+
+    const updateScrollIdx = (idx: number) => {
+        setScrollIdx(idx);
+    }
 
     const settingResources = (resources: Array<any>) => {
         setResources(resources)
     }
 
-    const scrollIndex = useRef<number>(0)
+    const scrollDiff = 400;
+    let windowWidth = window.innerWidth;
 
-    const scrollMethod = (arg: number, index:React.MutableRefObject<number>, partition: HTMLElement, container:HTMLElement, dif: number) => {
-        const calcScrollIndex = (arg: number, index:React.MutableRefObject<number>, dif: number, exceed: number) => {
-            if(((index.current + arg)*dif < exceed) && ((index.current+arg)*dif > 0)) index.current = index.current + arg
+    const scrollMethod = (arg: number, index:number, partition: HTMLElement, container:HTMLElement, dif: number) => {
+        const calcScrollIndex = (arg: number, index:number, dif: number, exceed: number) => {
+            let returnIdx = 0;
+            if(((index + arg)*dif < exceed) && ((index+arg)*dif > 0)) returnIdx = index + arg
             else {
-                if((index.current+arg)*dif <= 0) index.current = 0
-                else if((index.current+arg)*dif >= exceed) index.current = index.current + (exceed - index.current*dif)/dif
+                if((index +arg)*dif <= 0) returnIdx=0
+                else if((index+arg)*dif >= exceed) returnIdx = index + (exceed - index*dif)/dif
             }
-            return index.current
+            updateScrollIdx(returnIdx);
+            return returnIdx
         }
         const exceed = container.offsetWidth - partition.offsetWidth
-        container.setAttribute('style', `transform: translateX(-${dif * calcScrollIndex(arg, index, dif, exceed)}px)`)
+        const currentIdx = calcScrollIndex(arg, index, dif, exceed)
+        container.setAttribute('style', `transform: translateX(-${dif * currentIdx}px)`)
 
-        index.current !== 0 ? 
+        currentIdx !== 0 ? 
             document.getElementById(`left_button_${title}`)?.setAttribute('style', 'display: block') 
             : 
             document.getElementById(`left_button_${title}`)?.removeAttribute('style')
-        index.current === exceed/dif ?  
+        currentIdx === exceed/dif ?  
             document.getElementById(`right_button_${title}`)?.setAttribute('style', 'display: none') 
             :
             document.getElementById(`right_button_${title}`)?.removeAttribute('style')
 
     } 
 
+    const adjustOnWindowResizing = (previousWidth: number, scrollDiff: number, containerTranslated: React.MutableRefObject<number>) => {
+        const partition = document.getElementById(`partition_${title}`)
+        const container = document.getElementById(`container_${title}`)
+        if(window.innerWidth === partition?.offsetWidth && container) {
+            if(partition.getBoundingClientRect().right >= container.getBoundingClientRect().right - (scrollDiff * scrollIdx))
+            containerTranslated.current -= window.innerWidth - previousWidth 
+            container.setAttribute('style', `transform: translateX(-${containerTranslated.current}px)`)
+        } 
+        windowWidth = window.innerWidth;
+        console.log( scrollIdx, previousWidth, window.innerWidth, scrollDiff * scrollIdx, containerTranslated.current);
+    }
+
+    const adjustEventHandler = () => {
+        adjustOnWindowResizing(windowWidth, scrollDiff, containerTranslated)
+    }
+
     
 
     useEffect(()=>{
         loadArticles(title, 0, 5, settingResources)
-        
     },[])
+
+    //adjusting on window resizing
+    useEffect(()=>{
+        containerTranslated.current = scrollIdx * scrollDiff;
+        window.addEventListener('resize', adjustEventHandler)
+        return(()=>{window.removeEventListener('resize', adjustEventHandler)})
+    },[scrollIdx])
     return(
         <section className={styles.partition} id={`partition_${title}`}>
             <button className={`${styles.article_scroll} ` + styles.left} id={`left_button_${title}`} onClick={
                 ()=>{
-                    scrollMethod(-1, scrollIndex, document.getElementById(`partition_${title}`)!, document.getElementById(`container_${title}`)!, 400)
+                    scrollMethod(-1, scrollIdx, document.getElementById(`partition_${title}`)!, document.getElementById(`container_${title}`)!, scrollDiff)
                     }
             }>
                 {leftArrow()}
             </button>
             <button className={`${styles.article_scroll} `+ styles.right} id={`right_button_${title}`} onClick={
                 ()=>{
-                    scrollMethod(1, scrollIndex, document.getElementById(`partition_${title}`)!, document.getElementById(`container_${title}`)!, 400)
+                    scrollMethod(1, scrollIdx, document.getElementById(`partition_${title}`)!, document.getElementById(`container_${title}`)!, scrollDiff)
                     }
             } >
                     {rightArrow()}
