0e5d786a2a533c2b8e2641fcc4be27ba58efdd0e
taehyeungkim98 <kth1837@snu.ac.kr>
2023-03-09 11:55:06 +0000

    :sparkles: completed adding sidebar template with useContext

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,10 +7,14 @@ function Sidebar() {
     const appContext = useContext(AppContext)
 
     const hidingSidebar = (sidebar: HTMLDivElement) => { 
+        const sidebarCssAnimDuration = window.getComputedStyle(sidebar).getPropertyValue('animation-duration') as string;
+        const animDurationWithRegExp = sidebarCssAnimDuration.match(/[^s]+/i);
+        const animDurationFloat = animDurationWithRegExp ? parseFloat(animDurationWithRegExp[0]) : 0; 
         sidebar.classList.remove(styles['sidebar--appear']);
         sidebar.classList.add(styles['sidebar--disappear']);
-        const timer = setTimeout(appContext.context_sidebar.setter, 500) //timeout number === animation-duration
+        const timer = setTimeout(appContext.context_sidebar.setter, animDurationFloat * 1000) //timeout number === animation-duration
     }
+
 
     return(
     appContext.context_sidebar.visibility ? 
